436bd7eb9546a75b6c6a7dba93ffbbaf2f353b51
brivalin <baptiste.rivalin@gmail.com>
2017-04-07 14:44:16 +0200

    Modification colonne presence de smallInt à bit

--- src/main/java/fr/eni/nsy103/plateformeSupport/repository/RendezVousRepository.java
+++ src/main/java/fr/eni/nsy103/plateformeSupport/repository/RendezVousRepository.java
@@ -1,14 +1,19 @@
 package fr.eni.nsy103.plateformeSupport.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import fr.eni.nsy103.plateformeSupport.model.RendezVous;
+import fr.eni.nsy103.plateformeSupport.util.enums.Jours;
 
 @Repository
 public interface RendezVousRepository  extends JpaRepository<RendezVous,Long>{
 	@Query("Select rdv From RendezVous rdv where rdv.rdvPK.salarie.id = :salarieid and rdv.rdvPK.client.id = :clientid ")
-	public RendezVousRepository FindRendezVousRepositoryByIdsalarie(@Param("salarieid") int salarieId,@Param("clientid") int c);
+	public RendezVous findRendezVousRepositoryByIdsalarie(@Param("salarieid") int salarieId,@Param("clientid") int c);
+	
+	//public List<Jours> findRdvLibreParJour(@Param(""));
 }
